7f0b21400b2d670abd2273a6a66b04e92c9909ed
Tsukuyomi-Yomi <gamerm.2004121@gmail.com>
2025-12-01 00:40:45 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ $(function () {
         $(this).toggleClass('active');
     });
 });
+
+$(function () {
+    function end_loader() {
+        $('.loader').fadeOut(800);
+    }
+    $(window).on('load', function () {
+        setTimeout(function () {
+            end_loader();
+        }, 3000)
+    })
+})
